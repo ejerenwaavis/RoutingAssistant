3c4aaa0f574d84680cfdefe7e9184f9aaec06dbb
Avis <ejerenwaavis@gmailcom>
2021-03-21 00:48:54 -0400

    Stable v1

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,12 @@
+$(function(){
+  $("#roadWarrioirLink").hide();
+})
+
 function deleteFile(path){
   $.post("/delete", {path:path}, function(status){
-    if(200)
-    return console.log("sucessfull registered deletion");
+    if(200){
+      $("#roadWarrioirLink").fadeIn("fast").fadeOut("fast").fadeIn("slow");
+      return console.log("sucessfull registered deletion");
+    }
   });
 }
